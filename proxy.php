15b74208b29bd5078a714900cb4d4ce700cfc1bc
aishikaty <aishikaty@gmail.com>
2017-02-25 14:40:09 +0100

    Using $_GET for getting parameter value

--- proxy.php
+++ proxy.php
@@ -1,5 +1,4 @@
 <?php
-$url=preg_replace("/^.*?url=/","",$_SERVER['REQUEST_URI']);
-preg_match("/^https?:\/\/example.com/",$url,$matches);
-if($matches)readfile($url);
-?>
+if (preg_match("{^https?://example.com/}", $url = @$_GET['url'])) {
+  readfile($url);
+}
